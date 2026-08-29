1ea03611f44ea91103824f1b4745e8c648365d23
joeygauger <joey.gauger8@gmail.com>
2026-08-29 21:19:31 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='ci-step1-market-v2-141';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
